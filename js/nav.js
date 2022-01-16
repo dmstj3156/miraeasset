c69e1520e217cae7de5accbbf0f7f5d56368266e
dmstj3156 <84954658+dmstj3156@users.noreply.github.com>
2022-01-16 22:56:03 +0900

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,35 @@
+$(function(){
+    $('.depth1 > li').on('mouseover', function(){
+        $('.depth2, .nav_bg').addClass('show');
+        $('header,li a,header #family-site,.icon-menu > button >i,.depth1 > li a,header h1.sub-logo')
+        .addClass('show');
+        
+      })
+  
+      $('.depth1 > li').on('mouseout', function(){
+        $('.depth2, .nav_bg').removeClass('show');
+      
+        $('header,li a,header #family-site,.icon-menu > button >i, .depth1 > li a,header h1.sub-logo').removeClass('show');
+      
+       
+      })
+});
+
+
+$(function(){
+  $('#toggle').on('click', function(){
+    $('.depth2, .nav_bg').addClass('show');
+});
+
+// 메인메뉴 닫기
+$('.btn_close').on('click', function(){
+    $('.depth2, .nav_bg').removeClass('show');
+});
+
+// PC화면으로 넘어가면 모바일 메뉴 닫기
+$(window).on('resize', function(){
+  var x = window.matchMedia("(min-width: 768px)");
+  if(x.matches) { $('#gnb').removeClass('show');}
+})
+})
+
